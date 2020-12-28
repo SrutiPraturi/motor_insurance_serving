c9b88a865fef97bec3608f805be79ed0c8889d27
Sruti Sastry Praturi <Sruti_Praturi@infosys.com>
2020-12-28 14:08:09 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+import pandas as pd 
+import pickle
+import yaml
+import numpy as np 
+from flask import Flask, jsonify, request
+from explanation import explain_prediction
+import base64
+from feature_engg import model_ready_data
+
+app = Flask(__name__)
+
+lime_data = pd.read_csv('assets/insurance_data_processed.csv').drop(columns = ['Payment'])
+
+@app.route('/payment', methods = ['POST']) 
+def get_data():
+    
+    explain_data = request.data.decode()
+    
+    explain_data = explain_data.split(',')
+    print(request.data)
+    predict_data = []
+    for val in explain_data:
+        if val.isdigit() == True:
+            predict_data.append(int(val))
+        elif val.replace('.', '', 1).replace('"','',1).isdigit() == True:
+            predict_data.append(float(val))
+        else:
+            predict_data.append(str(val))
+            
+    file = open('assets/claims_model.pkl','rb')
+    model = pickle.load(file)
+    file.close()
+    
+    data = pd.DataFrame(columns = ['Kilometres','Zone','Bonus','Make','Insured','Claims'])
+    data.loc[0,:] = predict_data
+    model_data = model_ready_data(data)
+    
+    print(model_data)
+        
+    return_data = {}
+    
+    prediction = model.predict(np.array(model_data.iloc[0,:]).reshape(1,-1))
+    
+    return_data['prediction'] = str(np.expm1(prediction))
+    
+    result = explain_prediction(lime_data,model,model_data,'regression')
+    
+    
+    
+    if result == 1:
+        
+        with open('explanation.png', mode='rb') as file:
+            img = file.read()
+
+    return_data['img'] = base64.b64encode(img).decode()
+    
+    return_data = jsonify(return_data)
+        
+    
+    return return_data
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    #app.run(port=8080)
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ explanation.py
@@ -0,0 +1,37 @@
+import numpy as np 
+import pandas as pd 
+import lime
+import lime.lime_tabular
+import matplotlib.pyplot as plt
+import os 
+
+
+
+
+def explain_prediction(train_data , model , explain_data,mode):
+    
+    
+    #data = pd.read_csv(train_data)
+    feature_names = list(data.columns)
+    target = ['Payment']
+    categorical_features = []
+    
+    
+    for col in data.columns:
+        if data[col].dtypes == 'object' or data[col].dtypes =='bool':
+            categorical_features.append(col)
+            
+    explainer = lime.lime_tabular.LimeTabularExplainer(data.to_numpy(), feature_names=feature_names, class_names=target, categorical_features=categorical_features,  verbose=True, mode=mode)
+    
+    exp = explainer.explain_instance(explain_data.loc[0], model.predict, num_features=10)
+    
+    exp.as_pyplot_figure();
+    
+    plt.savefig('explanation.png')
+    
+    if os.path.exists('explanation.png'):
+        return 1
+    else :
+        return 0
+    
+    return 
